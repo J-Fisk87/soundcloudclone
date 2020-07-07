1930072b123de46bccc0de1765fb0216b8039ecf
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-07-07 11:41:53 -0500

    component and audio player lib

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ReactPlayer from "react-player";
+
+
+export default class Homepage extends Component {
+    state = {
+
+    }
+
+    render() {
+        return
+    }
+}
