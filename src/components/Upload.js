c13393616faef42181e161836fcd8a2d67323286
noahShafer <noahshafer0211@gmail.com>
2020-07-09 12:20:25 -0500

    Created basic track uploading

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { 
-    Menu,
+    Button,
+    Form,
     Container 
 
 } from 'semantic-ui-react'
@@ -8,13 +9,40 @@ import {
 
 
 export default class Upload extends Component {
-    state = {
-        user: {}
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        let formObj = {
+            user_id: this.props.user.id,
+            title: e.target.title.value,
+            audio_data: e.target.audio_data.files[0]
+        }
+        const data = new FormData()
+        Object.keys(formObj).forEach((key, value) => {
+            data.append(key, formObj[key])
+        })
+        fetch('http://localHost:3000/api/tracks/new', {
+            method: 'POST',
+            body: data
+        }).then(res => res.json()).then((json) => {
+            this.props.history.push('/home')
+        })
     }
  
     render() {
         return (
-            <div>Upload</div>
+            <Container text>
+                <Form encType="multipart/form-data" onSubmit={this.onSubmit}>
+                    <Form.Field>
+                    <label>Title</label>
+                    <input name="title" placeholder='Title' />
+                    </Form.Field>
+                    <Form.Field>
+                        <input type="file" name="audio_data" accept=".mp3,audio/*" />
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+            </Container>
         );
     }
 }
