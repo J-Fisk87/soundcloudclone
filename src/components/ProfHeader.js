dcc5dff3247f138d959329bd2f185fe055dd334d
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-07-09 12:03:13 -0500

    working on follow conditionals

--- src/components/ProfHeader.js
+++ src/components/ProfHeader.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Button, Icon } from "semantic-ui-react";
 
 export default function ProfHeader(props) {
-  let { username, email } = props.user;
+    let { username, email } = props.user;
+
+  function followButton() {
+    if (props.self){
+        return <Icon chess king size='large' />
+    els
+    }
+  }
+
   return (
     <div>
-      <p>User: {username}</p>
-  <p>@ {email}</p>
+      <span>User: {username} </span>
+      {/* <span>{followButton()}</span> */}
+      <p>@ {email}</p>
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,6 +13,7 @@ export default class Profile extends Component {
     this.state = {
       tracks: [],
       viewedUser: {},
+      selfProf: this.props.user.id === this.state.viewedUser.id
     };
   }
 
@@ -22,20 +23,18 @@ export default class Profile extends Component {
 
   // fetch tracks for viewed user, also set viewedUser
   // backend needs set up to render all tracks from single user
-  componentDidMount() {
-    fetch(
-      "/Users/J.Fisk/Development/code/SoundcloudClone-API/SoundcloudClone-API/public/ACM x NM - after thought 166 BPM.mp3"
-    ) // <-- temporary testing route
-      .then((r) => r.json())
-      .then((json) => this.setState({ tracks: json }));
+//   componentDidMount() {
+//     fetch(URL + "")
+//       .then((r) => r.json())
+//       .then((json) => this.setState({ tracks: json }));
     //   this.setState({viewedUser: })
-  }
+//   }
 
   render() {
     // let { user } = this.state.viewedUser;
     return (
       <Container text>
-        <ProfHeader user={this.props.user} /> <br />
+        <ProfHeader user={this.props.user} self={this.state.selfProf} /> <br />
         Tracks:
         <Tracklist tracks={this.state.tracks} />
       </Container>
